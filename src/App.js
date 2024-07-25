e249d3ce5bd5d203944ad61cf08ed647f6b8a612
Arthur Cechinel Nunes <an6281745@gmail.com>
2024-07-24 21:02:09 -0300

    ze-delivery-clone

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import PasswordPage from './pages/PasswordPage/PasswordPage'; 
-import ProductsPage from './pages/ProductsPage/ProductsPage'; 
+import PasswordPage from './pages/PasswordPage/PasswordPage';
+import ProductsPage from './pages/ProductsPage/ProductsPage';
+import AgeVerification from './components/AgeVerificationModal/AgeVerificationModal'; 
 
 const App = () => {
+  const [isVerified, setIsVerified] = useState(false);
   const location = useLocation();
   const showHeader = location.pathname === '/';
+
+  const handleVerification = () => {
+    setIsVerified(true);
+  };
+
+  if (!isVerified) {
+    return <AgeVerification onConfirm={handleVerification} />;
+  }
 
   return (
     <div>
@@ -16,9 +26,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<Login />} /> {}
         <Route path="/password" element={<PasswordPage />} />
-        <Route path="/produtos/:produtoNome?" element={<ProductsPage />} /> {}
+        <Route path="/produtos/:produtoNome?" element={<ProductsPage />} />
       </Routes>
     </div>
   );
